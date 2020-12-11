be8d4595766499ef56d10c764f58fc812494692d
Adrian Ałaszewski <wns2148810@interia.pl>
2020-12-11 13:29:39 +0100

    Split file

---
+++ 8/main.js
@@ -0,0 +1,254 @@
+function run() {
+    const myConsole = document.getElementById('myConsole');
+    const printToScreen = (msg) => {
+        myConsole.innerHTML = msg;
+    }
+    // Prevent context menu on button press and hold activity
+    document.querySelectorAll('button').forEach(element => {
+        element.addEventListener('contextmenu', event => event.preventDefault());
+    });
+
+
+
+    let canvas = document.getElementById("myCanvas");
+    let ctx = canvas.getContext("2d");
+
+    let start;
+
+    const player1 = new Player(100, 570)
+
+    const userInput = {
+        directionX: 0,
+        acceleration: 0,
+        isShooting: false
+    }
+
+    const turnLeft = () => {
+        userInput.directionX = -1
+    }
+
+    const turnRight = () => {
+        userInput.directionX = 1
+    }
+
+    const stopTurning = () => {
+        userInput.directionX = 0
+    }
+
+    const shoot = () => {
+        userInput.isShooting = true
+    }
+
+    const stopShooting = () => {
+        userInput.isShooting = false
+    }
+
+//printToScreen("works");
+
+    const handleKeyDown = (event) => {
+        switch (event.key) {
+            case "ArrowRight": {
+                event.preventDefault()
+                turnRight()
+                break
+            }
+            case "ArrowLeft": {
+                event.preventDefault()
+                turnLeft()
+                break
+            }
+            case " ": {
+                event.preventDefault()
+                shoot()
+                break
+            }
+        }
+    }
+
+    const handleKeyUp = (event) => {
+        event.preventDefault();
+        switch (event.key) {
+            case "ArrowRight": {
+                stopTurning();
+                break;
+            }
+            case "ArrowLeft": {
+                stopTurning();
+                break;
+            }
+            case " ": {
+                stopShooting();
+                break;
+            }
+        }
+    }
+
+    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('keyup', handleKeyUp);
+
+    const handleTouch = () => {
+        shoot();
+    }
+
+    canvas.addEventListener("touchstart", handleTouch, false);
+
+    let score = 0;
+
+    function isInRange(value, a, b) {
+        return value >= a && value <= b;
+    }
+
+    function isOverlapping(object, target) {
+        return (
+                isInRange(target.position.y, object.position.y, object.position.y + object.size.y) ||
+                isInRange(target.position.y + target.size.y, object.position.y, object.position.y + object.size.y)
+            ) &&
+            (
+                isInRange(target.position.x, object.position.x, object.position.x + object.size.x) ||
+                isInRange(target.position.x + target.size.x, object.position.x, object.position.x + object.size.x)
+            );
+    }
+
+    function randomRoadX() {
+        return Math.random() * (canvas.width - 125) + 25;
+    }
+
+    let targets = new Set();
+
+    function addTargetsWave(offset) {
+        for (let i = 0; i < 16; i++) {
+            targets.add(new Obstacle(i * 50, offset));
+        }
+    }
+
+    let speed = 4
+
+    bullets = new Set()
+
+    let gameOver = false
+
+
+    function updatePlayer(player) {
+        if (userInput.isShooting) {
+            player.shoot(bullets)
+            stopShooting()
+        }
+
+        player.move(userInput.directionX, canvas.width)
+        if (orientY !== 0) {
+            player.move(Math.min(Math.max(orientY / 20, -1), 1), canvas.width)
+        }
+
+        ctx.beginPath()
+        ctx.drawImage(player.img, player.position.x, player.position.y)
+    }
+
+    function updateScore(score) {
+        ctx.font = "30px Arial"
+        ctx.fillStyle = "#d5d5d5"
+        ctx.fillText(`Score: ${score}`, 10, 30)
+    }
+
+    function printGameOver(msg) {
+        ctx.font = "40px Arial"
+        ctx.strokeText(msg, canvas.width / 2 - 200, canvas.height / 2)
+    }
+
+    function updateBullets(bullets) {
+        bullets.forEach(bullet => {
+            bullet.move();
+            targets.forEach(target => {
+                if (isOverlapping(bullet.getCollisionBox(), target) || isOverlapping(target, bullet.getCollisionBox())) {
+                    targets.delete(target)
+                    score++;
+                }
+            });
+
+            if (isOverlapping(bullet.getCollisionBox(), player1) || isOverlapping(player1, bullet.getCollisionBox())) {
+                bullet.speed.y = Math.abs(bullet.speed.y);
+            }
+
+            if (bullet.position.y > canvas.height) {
+                gameOver = true;
+            }
+            if (bullet.position.y < 0) {
+                bullet.speed.y *= -1;
+            }
+
+
+            ctx.beginPath()
+            ctx.fillStyle = bullet.color
+            ctx.arc(bullet.position.x, bullet.position.y, bullet.radius, 0, 2 * Math.PI)
+            ctx.fill()
+        })
+    }
+
+
+    let orientX = 0;
+    let orientY = 0;
+
+    function handleOrientation(event) {
+        orientX = event.beta;
+        orientY = event.gamma;
+    }
+
+    function permission() {
+        if (typeof (DeviceMotionEvent) !== "undefined" && typeof (DeviceMotionEvent.requestPermission) === "function") {
+            DeviceMotionEvent.requestPermission()
+                .then(response => {
+                    if (response === "granted") {
+                        window.addEventListener('deviceorientation', handleOrientation, true);
+                    }
+                })
+                .catch(console.error)
+        } else {
+            window.addEventListener('deviceorientation', handleOrientation, true);
+        }
+    }
+
+    permission();
+
+    function init() {
+        addTargetsWave(30);
+        setInterval(() => {
+            console.log("Test");
+
+        }, 5000);
+    }
+
+    function updateTargets(targets) {
+        targets.forEach(target => {
+            ctx.beginPath();
+            ctx.drawImage(target.img, target.position.x, target.position.y);
+        });
+    }
+
+    function animate(timestamp) {
+        if (start === undefined) {
+            start = timestamp
+            init();
+        }
+        const elapsed = (timestamp - start);
+        const elapsedSeconds = Math.floor(elapsed / 1000)
+        if (gameOver) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height)
+            ctx.fillStyle = "#FF0000"
+            ctx.fillRect(0, 0, canvas.width, canvas.height)
+            printGameOver(`GAME OVER! SCORE: ${score}`)
+        } else {
+            //if(!turn && (elapsedSeconds + 12) % 15 === 0) turn = true;
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.fillStyle = "#0a0f3d";
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            updateBullets(bullets);
+            updateTargets(targets);
+            updatePlayer(player1);
+            updateScore(score);
+        }
+        window.requestAnimationFrame(animate);
+    }
+
+    window.requestAnimationFrame(animate);
+}
+
+document.addEventListener('DOMContentLoaded', run);
---
+++ 8/objects.js
@@ -0,0 +1,86 @@
+class GameObject {
+    constructor() {
+        this.position = {
+            x: 0,
+            y: 0
+        }
+    }
+
+    setPosition(x, y) {
+        if (x !== undefined) this.position.x = x
+        if (y !== undefined) this.position.y = y
+    }
+}
+
+class Player extends GameObject {
+    constructor(posX, posY) {
+        super()
+        this.speed = 5
+        this.img = new Image()
+        this.img.src = "spaceship.png"
+        this.size = {
+            x: 50,
+            y: 10
+        }
+        this.setPosition(posX, posY)
+    }
+
+    move(directionX, mapWidth) {
+        let clampedPositionX = Math.min(this.position.x + (this.speed * directionX), mapWidth - this.size.x);
+        clampedPositionX = Math.max(clampedPositionX, 0);
+        this.setPosition(clampedPositionX, this.position.y);
+    }
+
+    shoot(bullets) {
+        if (bullets.size < 1) {
+            bullets.add(
+                new Bullet(this.position.x + (this.size.x / 2), this.position.y - (this.size.y / 2) - 10)
+            );
+        }
+    }
+}
+
+class Obstacle extends GameObject {
+    constructor(posX, posY) {
+        super();
+        this.color = "#ffb0b0"
+        this.img = new Image()
+        this.img.src = "target.png"
+        this.size = {
+            x: 50,
+            y: 20
+        }
+        this.setPosition(posX, posY)
+    }
+}
+
+
+class Bullet extends GameObject {
+    constructor(posX, posY) {
+        super();
+        this.setPosition(posX, posY)
+        this.speed = {
+            x: 0,
+            y: 8
+        }
+        this.radius = 10
+        this.color = "#9c6107"
+    }
+
+    getCollisionBox() {
+        return {
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            size: {
+                x: this.radius * 2,
+                y: this.radius * 2
+            }
+        }
+    }
+
+    move() {
+        this.setPosition(this.position.x - this.speed.x, this.position.y - this.speed.y)
+    }
+}
