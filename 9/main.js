3faa20c65c9422c7a73c75956015821e889cb2f2
Adrian Ałaszewski <wns2148810@interia.pl>
2021-01-13 12:12:02 +0100

    Map generation

--- 9/main.js
+++ 9/main.js
@@ -72,16 +72,104 @@ function run() {
     let winZone = null;
     const walls = new Set();
 
-    function startGame() {
-        permission();
+    function generateMap(seed) {
+        const s = seed ? seed : Math.floor(Math.random() * 10);
+        console.log(`s${s}`);
+        if(s % 2 === 0) {
+            player = new Player(50, 50);
+            winZone = new WinZone(canvas.width - 50, canvas.height - 50);
+        } else {
+            player = new Player(canvas.width - 50, canvas.height - 50);
+            winZone = new WinZone(50, 50);
+        }
 
         walls.add(new Wall(200, 200, 100, 10));
         walls.add(new Wall(350, 400, 10, 100));
         walls.add(new Wall(700, 500, 100, 10));
+        walls.add(new Wall(canvas.width - 100, 0, 10, 100));
 
-        player = new Player(100, 100);
-        winZone = new WinZone(500, 500);
+        if(s % 2 === 1) {
+            walls.add(new Wall(canvas.width - 200, canvas.height - 200, 10, 200));
+            walls.add(new Wall(150, 400, 200, 10));
+            walls.add(new Wall(100, 0, 10, 200));
+        } else {
+            walls.add(new Wall(300, canvas.height - 200, 10, 200));
+            walls.add(new Wall(100, canvas.height - 200, 200, 10));
+            walls.add(new Wall(300, 100, 200, 10));
+        }
 
+        if(s === 1 || s === 4) {
+            walls.add(new Wall(canvas.width - 150, 300, 150, 10));
+            walls.add(new Wall(canvas.width - 150, 300, 10, 50));
+            walls.add(new Wall(canvas.width - 100, 250, 10, 50));
+        } else if(s === 2 || s === 8 || s === 9) {
+            walls.add(new Wall(canvas.width - 150, 300, 150, 10));
+            walls.add(new Wall(canvas.width - 150, 250, 150, 10));
+        } else if(s > 5) {
+            walls.add(new Wall( 400, canvas.height - 100, 100, 10));
+            walls.add(new Wall( 500, canvas.height - 100, 10, 100));
+            walls.add(new Wall( canvas.width - 250, canvas.height - 150, 10, 100));
+            walls.add(new Wall( canvas.width - 250, canvas.height - 150, 50, 10));
+        }
+        if(s === 3 || s === 7) {
+            walls.add(new Wall( 200, 300, 400, 10));
+            walls.add(new Wall( 650, 300, 100, 10));
+        } else {
+            walls.add(new Wall(400, 50, 10, 350));
+            walls.add(new Wall(450, 450, 10, 150));
+        }
+        if(s === 0 || s === 9) {
+            walls.add(new Wall(0, 300, 100, 10));
+            walls.add(new Wall(400, 300, 100, 10));
+            walls.add(new Wall(400, 100, 150, 10));
+            walls.add(new Wall(canvas.width - 100, canvas.height - 150, 100, 10));
+            walls.add(new Wall(canvas.width - 100, canvas.height - 250, 10, 100));
+            walls.add(new Wall(canvas.width - 300, canvas.height - 250, 200, 10));
+        }
+        if(s !== 9 && s !== 8) {
+            walls.add(new Wall(canvas.width - 50, 300, 50, 10));
+            if(s <= 5) {
+                walls.add(new Wall(canvas.width - 150, 300, 50, 10));
+            }
+        }
+        if(s === 3) {
+            walls.add(new Wall(400, 50, 10, 250));
+            walls.add(new Wall(450, 300, 10, 50));
+            walls.add(new Wall(600, 250, 10, 100));
+            walls.add(new Wall(650, 250, 10, 50));
+            walls.add(new Wall(100, 250, 10, 100));
+            walls.add(new Wall(150, 400, 10, 100));
+            walls.add(new Wall(50, canvas.height - 100, 10, 100));
+        } else if(s === 6) {
+            walls.add(new Wall(0, 300, 150, 10));
+            walls.add(new Wall(150, 200, 10, 110));
+            walls.add(new Wall(canvas.width - 200, 100, 110, 10));
+        } else  if(s === 8) {
+            walls.add(new Wall(200, 200, 10, 100));
+            walls.add(new Wall(100, 300, 110, 10));
+            walls.add(new Wall(0, 200, 100, 10));
+        } else if(s === 2) {
+            walls.add(new Wall(0, 100, 100, 10));
+            walls.add(new Wall(100, 200, 100, 10));
+            walls.add(new Wall(200, 300, 200, 10));
+            walls.add(new Wall(400, 400, 200, 10));
+            walls.add(new Wall(590, 300, 10, 100));
+            walls.add(new Wall(500, 300, 100, 10));
+        } else if(s === 1) {
+            walls.add(new Wall(500, 0, 10, 200));
+            walls.add(new Wall(450, 190, 50, 10));
+            walls.add(new Wall(150, 500, 210, 10));
+        } else if(s === 7) {
+            walls.add(new Wall(400, 0, 10, 250));
+            walls.add(new Wall(450, 50, 10, 250));
+            walls.add(new Wall(500, 300, 10, 150));
+            walls.add(new Wall(0, 550, 150, 10));
+        }
+    }
+
+    function startGame() {
+        permission();
+        generateMap();
         gameStarted = true;
     }
 
@@ -175,7 +263,7 @@ function run() {
                 printOnScreen(`YOU WIN!`);
             } else {
                 ctx.clearRect(0, 0, canvas.width, canvas.height);
-                ctx.fillStyle = "#a7b1ff";
+                ctx.fillStyle = "#2f3daa";
                 ctx.fillRect(0, 0, canvas.width, canvas.height);
                 // debugCollision();
                 updateWalls();
